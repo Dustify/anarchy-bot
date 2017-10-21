3f596d54b8af3d1bf37ce28c88c5cefcc447abd0
Adrian Johnson <adrian@cqdev.co.uk>
2017-10-21 23:06:44 +0100

    updated nuget packages, added looping try catch around main

--- AnarchyBot/Program.cs
+++ AnarchyBot/Program.cs
@@ -1,11 +1,27 @@
 ﻿namespace AnarchyBot
 {
+    using System;
+    using System.Threading;
+
     public class Program
     {
+        private const int RestartDelayMilliseconds = 60000;
+
         public static void Main(string[] args)
         {
-            // simply call the manager, we're expecting it to stop the thread from finishing
-            var manager = new Manager();
+            while (true)
+            {
+                try
+                {
+                    // simply call the manager, we're expecting it to stop the thread from finishing
+                    var manager = new Manager();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Unhandled exception: {exception.Message}, restarting in {RestartDelayMilliseconds}ms");
+                    Thread.Sleep(RestartDelayMilliseconds);
+                }
+            }
         }
     }
 }
